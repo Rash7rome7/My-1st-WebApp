df45572ebcccdb40a41cac26e17b35803ae5921e
Rashmika De Alwis <rashmika_dealwis@yahoo.com>
2016-08-13 19:35:32 +1200

    Added Flicar image puler pen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('\'Allo \'Allo!');
+
+// Pull photos from Flickr
+ 
+$.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?id=96272396@N05&size=b&lang=en-us&format=json&jsoncallback=?', function(data){
+		$.each(data.items, function(i,item){
+			 var image = (item.media.m).replace('_m.jpg', '_d.jpg');
+       var title = item.title;
+       if(i<21){
+			    $('<div>')
+          .attr({'style':'background-image: url('+image+')','class':'flickr-image', 'data-title':title})
+           .appendTo('.flickr').wrap('<a href=\'' + item.link + '\'></a>');
+			    }    
+		});
+});
